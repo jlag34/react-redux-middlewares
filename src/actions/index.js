8a64269d066f7ab7de841a9720a0b609d8e68c39
Joe Lagasse <joe@servant.co>
2016-04-09 12:13:08 -0400

    Finish FETCH_USER action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-//actions home
+import {FETCH_USERS} from './types';
+
+export function fetchUsers() {
+  return {
+    type: FETCH_USERS,
+    payload: [
+      {name: 'Jane'},
+      {name: 'Alex'},
+      {name: 'Jim'}
+    ]
+  };
+}
