2e3808adbf125fc88a379c0cf8794dc61469d4cd
Joe Lagasse <joe@servant.co>
2016-04-09 13:17:40 -0400

    Complete custom middleware that unwraps promises

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,17 @@
 export default function({dispatch}){
   return next => action => {
-    console.log(action);
+    // if action does not have a payload or does not have a .then property
+    if(!action.payload || !action.payload.then) {
+      return next(action);
+    }
     
-    next(action);
+    // make sure action's promise resolves
+    action.payload
+      .then(response => {
+        // create a new action with old type but replace promise with data
+        const newAction = {...action, payload: response}
+      // put new data through all middlewares again
+      dispatch(newAction);    
+    });
   }
 }
